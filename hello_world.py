c7e1b62e9f1ab6438946c0c00d4ce8b26df62bd0
aikilink <quintinrg@protonmail.com>
2019-11-21 13:07:51 -0600

    Succesfully writing input data in json form to json file. Need to adjust so it doesn't make new category for every submission.

--- hello_world.py
+++ hello_world.py
@@ -2,34 +2,34 @@ from flask import render_template
 from flask import Flask
 app = Flask(__name__)
 
-from random import randint
+# from random import randint
 
-my_rand = randint(0, 99)
-print(f'random: {my_rand}')
-top_limit = 100
-bottom_limit = 0
-guess = " "
-tries = 0
+# my_rand = randint(0, 99)
+# print(f'random: {my_rand}')
+# top_limit = 100
+# bottom_limit = 0
+# guess = " "
+# tries = 0
 
-while my_rand != guess:
-  tries = tries + 1
+# while my_rand != guess:
+#   tries = tries + 1
 
-  guess = int((top_limit + bottom_limit) / 2)
+#   guess = int((top_limit + bottom_limit) / 2)
 
-  print(f'Guess: {guess}, Toplimit: {top_limit}, Bottom limit: {bottom_limit}')
+#   print(f'Guess: {guess}, Toplimit: {top_limit}, Bottom limit: {bottom_limit}')
 
-  if guess > my_rand:
-    print("Guess was too high")
-    top_limit = guess
+#   if guess > my_rand:
+#     print("Guess was too high")
+#     top_limit = guess
 
-  elif guess < my_rand:
-    print("Guess was too low")
-    bottom_limit = guess
+#   elif guess < my_rand:
+#     print("Guess was too low")
+#     bottom_limit = guess
 
-  elif guess == my_rand:
-    print(f"Correct, the number was {my_rand}!")
-    final_answer = guess
-    final_tries = tries
+#   elif guess == my_rand:
+#     print(f"Correct, the number was {my_rand}!")
+#     final_answer = guess
+#     final_tries = tries
 
 
 @app.route("/")
--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-def greet():
-  print('hi world')
+import json
+
+rec_name = input("What is the name of the recipe?  ")
+
+web_adr = input("Please input the url of the recipe.  ")
+
+ing_one = input("What is the most prominent ingredient?  ")
+
+ing_two = input("Secondary noteworthy ingredient?  ")
+
+complexity = input("Is it high, medium, or low complexity?  ")
+
+fanciness = input("Is it high, medium, or low fanciness?  ")
+
+data = {}
+data['recipes'] = []
+data['recipes'].append(
+    {
+        'name': rec_name,
+        'link': web_adr,
+        'ingred 1': ing_one,
+        'ingred 2': ing_two,
+        'complex': complexity,
+        'fancy': fanciness
+    }
+)
+
+with open('data.json', 'a') as data_file:
+  json.dump(data, data_file)
