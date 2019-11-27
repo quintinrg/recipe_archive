c654998ff8767a7568b0870e60b8bd9d00ab570b
aikilink <quintinrg@protonmail.com>
2019-11-26 23:12:07 -0600

    Added PUT and DELETE logic for https. Still need to make it functional.

--- hello_world.py
+++ hello_world.py
@@ -3,41 +3,50 @@ import json
 import uuid
 app = Flask(__name__)
 
-with open("data.json", 'r') as data_file:
+with open('data.json', 'r') as data_file:
   data = json.load(data_file)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  if request.method == "GET":
+  if request.method == 'GET':
     return jsonify(data)
-  elif request.method == "POST":
+
+  elif request.method == 'POST':
     body = request.get_json()
 
-    body["id"] = str(uuid.uuid4())
+    body['id'] = str(uuid.uuid4())
     data.append(body)
     with open('data.json', 'w') as data_file:
       json.dump(data, data_file, indent=2)
-    return next(recipe for recipe in data if recipe["id"] == body["id"])
+    return next(recipe for recipe in data if recipe['id'] == body['id'])
 
     #Delete Route
     # Update Route
 
 
 #gets single recipe
-@app.route("/<id>")
+@app.route('/<id>', methods=['GET', 'DELETE', 'PUT'])
 def single_recipe(id):
+  if request.method == 'GET':
+    return next(recipe for recipe in data if recipe['id'] == id)
+
+  elif request.method == 'DELETE':
+    return "Deleted"
+
+  elif request.method == 'PUT':
+    return "Updated"
+
   #add if else for PUT(update) and DELETE and GET(single)
-  return next(recipe for recipe in data if recipe["id"] == id)
 
 
-#1 use actual database - later
+#1 build frontend - React/TS
 
-#2 build frontend
+#2 use actual database - later
 
-# docker deployed to digitalocean
+#3 docker deployed to digitalocean
 
 #rethink db (noSQL)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   app.run()
